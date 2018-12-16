3f094651a2d84d13568605d75e2607c7b87efd93
jack-nie <hust.ud.hui@gmail.com>
2018-12-16 22:39:37 +0800

    add perf

--- 21.go
+++ 21.go
@@ -19,3 +19,30 @@ func reorderOddEven(nums []int) []int {
 	}
 	return nums
 }
+
+func reorderOddEvenTwo(nums []int, f func(int) bool) []int {
+	if len(nums) == 0 {
+		return nil
+	}
+	start := 0
+	end := len(nums) - 1
+	for start < end {
+		for start < end && isEven(nums[start]) {
+			start++
+		}
+		for start < end && !isEven(nums[end]) {
+			end--
+		}
+		if start < end {
+			nums[start], nums[end] = nums[end], nums[start]
+		}
+	}
+	return nums
+}
+
+func isEven(i int) bool {
+	if i&0x1 != 0 {
+		return true
+	}
+	return false
+}
--- 21_test.go
+++ 21_test.go
@@ -19,3 +19,19 @@ func TestReorderOddEven(t *testing.T) {
 		}
 	}
 }
+
+func TestReorderOddEvenTwo(t *testing.T) {
+	for _, v := range []struct {
+		nums   []int
+		fn     func(n int) bool
+		wanted []int
+	}{
+		{[]int{1, 2, 3, 4, 5, 6}, isEven, []int{1, 5, 3, 4, 2, 6}},
+		{nil, isEven, nil},
+	} {
+		got := reorderOddEvenTwo(v.nums, isEven)
+		if !reflect.DeepEqual(got, v.wanted) {
+			t.Errorf("Failed, expected %d, got %d", v.wanted, got)
+		}
+	}
+}
