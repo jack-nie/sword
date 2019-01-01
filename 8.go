b9e8dc87d9b919deeb1bf57f0d5cd7a73aadf01f
jack-nie <hust.ud.hui@gmail.com>
2019-01-01 10:46:09 +0800

    find next node of current node

---
+++ 8.go
@@ -0,0 +1,24 @@
+package algo
+
+func getNext(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	var next *TreeNode
+	if root.Right != nil {
+		right := root.Right
+		for right.Left != nil {
+			right = right.Left
+			next = right
+		}
+	} else if root.Parent != nil {
+		current := root
+		parent := current.Parent
+		for parent != nil && current == parent.Right {
+			current = parent
+			parent = parent.Parent
+		}
+		next = parent
+	}
+	return next
+}
